0b1269b1cfecf7317925a3ef5ce16cfbcdbf7b8f
DianaOlvera <diana.saamaa@gmail.com>
2018-10-19 13:39:31 -0500

    agregando componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { MuroComponent } from './muro/muro.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import {FirebaseModule, FirebaseProvider} from 'angular-firebase'
+import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuroComponent,
+    LoginComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    //AngularFireModule,
+    NgbModule,
+    FirebaseModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+AngularFireModule : {
+    apiKey: "AIzaSyDzUkvNCmsmAbqbI4xF1CeeLSpPiOLeHYg",
+    authDomain: "social-network-6eb8a.firebaseapp.com",
+    databaseURL: "https://social-network-6eb8a.firebaseio.com",
+    projectId: "social-network-6eb8a",
+    storageBucket: "social-network-6eb8a.appspot.com",
+    messagingSenderId: "250841734328"
+  }
 };
 
 /*
